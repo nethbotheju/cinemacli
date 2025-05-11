dcedfc3514e18501f1042d9c2aa746659223cf8e
Neth Botheju <nethbotheju@gmail.com>
2025-05-12 01:16:10 +0530

    feat: add input prompts and selection for movies and TV series

--- index.js
+++ index.js
@@ -1,2 +1,99 @@
-const name = process.argv[2] || "stranger";
-console.log(`Hello, ${name}!`);
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+
+async function main() {
+  const { contentType } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "contentType",
+      message: "Select a content type:",
+      choices: ["Movie", "TV Series"],
+    },
+  ]);
+
+  if (contentType === "Movie") {
+    const { movieName, movieYear, movieQuality } = await movie();
+
+    console.log("\n🎬 Movie Information:");
+    console.log("Name:", movieName);
+    console.log("Year:", movieYear);
+    console.log("Quality:", movieQuality);
+  } else {
+    const { seriesName, seriesSeason, seriesEpisode, seriesQuality } =
+      await tvSeries();
+    console.log("\n📺 TV Series Info:");
+    console.log("Name:", seriesName);
+    console.log("Season:", seriesSeason);
+    console.log("Episode:", seriesEpisode);
+    console.log("Quality:", seriesQuality);
+  }
+}
+
+async function movie() {
+  const { movieName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "movieName",
+      message: "Enter the name of the movie:",
+    },
+  ]);
+
+  const { movieYear } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "movieYear",
+      message: "Enter the year of the movie:",
+    },
+  ]);
+
+  const { movieQuality } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "movieQuality",
+      message: "Select the quality you need to watch:",
+      choices: ["360p", "480p", "720p", "1080p"],
+    },
+  ]);
+
+  return { movieName, movieYear, movieQuality };
+}
+
+async function tvSeries() {
+  const { seriesName } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "seriesName",
+      message: "Enter the name of the TV series:",
+    },
+  ]);
+
+  const { seriesSeason } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "seriesSeason",
+      message: "Enter the season number:",
+    },
+  ]);
+
+  const { seriesEpisode } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "seriesEpisode",
+      message: "Enter the episode number:",
+    },
+  ]);
+
+  const { seriesQuality } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "seriesQuality",
+      message: "Select the quality you need to watch:",
+      choices: ["360p", "480p", "720p", "1080p"],
+    },
+  ]);
+
+  return { seriesName, seriesSeason, seriesEpisode, seriesQuality };
+}
+
+main();
